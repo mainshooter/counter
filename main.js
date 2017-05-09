5b39a614703b73ece5dc91a4dbd2525b751908ac
Peter 4A5A <498883@edu.rocmn.nl>
2017-05-09 12:23:16 +0200

    Centered the div

--- main.js
+++ main.js
@@ -14,14 +14,18 @@ function counter() {
   }
 }
 function setStartStyle() {
+  // Set the style
   counterDisplay.style.backgroundColor = "#2c3e50";
-  // counterDisplay.style.width = "";
+  counterDisplay.style.width = "1em";
   counterDisplay.style.padding = "1em";
   counterDisplay.style.fontSize = "1em";
-  // counterDisplay.style.height = "";
+  counterDisplay.style.height = "1em";
   counterDisplay.style.position = "absolute";
-  counterDisplay.style.top = "50%";
-  counterDisplay.style.left = "50%";
+  counterDisplay.style.top = "0";
+  counterDisplay.style.left = "0";
+  counterDisplay.style.bottom = "0";
+  counterDisplay.style.right = "0";
+  counterDisplay.style.margin = "auto";
   counterDisplay.style.color = "#fff";
   counterDisplay.style.textAlign = "center";
   counterDisplay.style.borderRadius = "50%";
